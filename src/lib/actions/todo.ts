50ad1b3673e6ef52c45a4a99fd1d98a0f64070ad
mohak <c.mohak2004@gmail.com>
2025-03-05 00:04:57 +0530

    route updates

--- src/lib/actions/todo.ts
+++ src/lib/actions/todo.ts
@@ -2,7 +2,6 @@
 import prisma from "@/db";
 import { getServerSession } from "next-auth";
 import authOptions from "./authOptions";
-import { Staatliches } from "next/font/google";
 
 
 // create a list 
@@ -76,7 +75,88 @@ export async function addTask(title : string, listId : string ,description? : st
 
 // fetch all tasks of active lists
 
+export async function fetchActive() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
 
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
 
+    try {
+        const response = await prisma.todoList.findFirst({
+            where : {
+                creatorId : userId
+            }, 
+            select : {
+                id : true,
+                Title : true,
+                tasks : true
+            }
+        })
+
+        // return in better format 
+        return {
+            title : response?.Title,
+            id : response?.id,
+            tasks : response?.tasks // array of tasks type
+        }
+
+        // another approach ===============>>>>>>>
+
+        // const response1 = await prisma.todoList.findFirst({
+        //     where : {
+        //         creatorId : userId
+        //     }, 
+        //     select : {
+        //         id : true,
+        //     }
+        // })
+        // const listId = response1?.id
+        // const tasks = await prisma.tasks.findMany({
+        //     where : {
+        //         ListId : listId
+        //     }
+        // })
+        // return tasks;
+
+    } catch (error) {
+        console.log (error); 
+        return {
+            msg : "some error occurred", 
+            status : 502
+        }
+    }
+}
 
 // fetch all lists 
+export async function allLists() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
+
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
+
+    try {
+        const Lists = await prisma.todoList.findMany({
+            where : {
+                creatorId : userId
+            }
+        })
+
+        // return better format 
+
+        return Lists;
+    } catch (error) {
+        console.log (error); 
+        return {
+            msg : "some error occurred", 
+            status : 502
+        }
+    }
+}
