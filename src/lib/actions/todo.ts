8e0b09150205067d75e300b044e9a5fbe404f7fc
mohak <c.mohak2004@gmail.com>
2025-03-05 19:32:46 +0530

    API updates

--- src/lib/actions/todo.ts
+++ src/lib/actions/todo.ts
@@ -16,6 +16,20 @@ export async function createList(title : string) {
     }
 
     try {
+
+        const ActiveList = await prisma.todoList.findFirst({
+            where : {
+                creatorId : userId,
+                completed : true
+            } 
+        })
+
+        if (ActiveList) {
+            return {
+                msg : "One list Already exists, finish before creating another"
+            }
+        }
+
         const response = await prisma.todoList.create({
             data :{
                 Title : title,
@@ -156,6 +170,171 @@ export async function allLists() {
         console.log (error); 
         return {
             msg : "some error occurred", 
+            status : 502
+        }
+    }
+}
+
+// updating todos and lists 
+
+// when called on frontend send listId with it
+export async function updateList(listId : string) {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
+
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
+
+    try {
+        const response = await prisma.todoList.update({
+            where : {
+                id : listId
+            } ,
+            data : {
+                completed : true,
+                EndTime : Date()
+            }
+        })
+
+        return {
+            msg  : "list updated",
+            response
+        }
+    } catch (error) {
+        console.log (error); 
+        return {
+            msg : "some error occurred", 
+            status : 502
+        }
+    }
+}
+
+export async function updateTask(TaskId :string) {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
+
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
+
+    try {
+        // should add if the task completed was the last task then automatically complete the list
+        const response = await prisma.tasks.update({
+            where : {
+                id : TaskId
+            } ,
+            data : {
+                completed : true,
+                endTime : Date()
+            }
+        })
+
+        return {
+            msg  : "list updated",
+            response
+        }
+    } catch (error) {
+        console.log (error); 
+        return {
+            msg : "some error occurred", 
+            status : 502
+        }
+    }
+}
+
+// deleting list and task 
+
+export async function deleteList (listId: string) {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
+
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
+
+    try {
+        // const response1 = await prisma.tasks.deleteMany({
+        //     where : {
+        //         ListId : {
+        //             contains : listId
+        //         }
+        //     }
+        // })
+        // const response2 = await prisma.todoList.delete({
+        //     where : {
+        //         id : listId
+        //     }
+        // })
+
+        // if (!response1 || !response2) {
+        //     return 
+        // }
+
+       const [res1, res2] = await prisma.$transaction([
+
+             prisma.tasks.deleteMany({
+                where : {
+                    ListId : {
+                        contains : listId
+                    }
+                }
+            }) ,
+
+            prisma.todoList.delete({
+                where : {
+                    id : listId
+                }
+            })
+
+        ])
+
+        return {
+            msg : "List and Tasks deleted",
+            res1, 
+            res2
+        }
+
+    } catch (error) {
+        console.log(error);
+        return {
+            msg : "some error occured",
+            status : 502
+        }
+    }
+}
+
+export async function deleteTask(TaskId : string) {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id 
+
+    if (!userId) {
+        return {
+            msg : "invalid access please login"
+        }
+    }
+
+    try {
+        const response  = await prisma.tasks.delete({
+            where : {
+                id : TaskId
+            }
+        })
+
+        return {
+            msg : "task deleted",
+            response
+        }
+    } catch (error) {
+        console.log(error);
+        return {
+            msg : "some error occured",
             status : 502
         }
     }
